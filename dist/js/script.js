9e1ca31dea2c4c254b21eebb3f3efa99b15c76df
samzdinz <samzdinz@gmail.com>
2022-10-17 14:04:01 +0700

    update image profile

--- dist/js/script.js
+++ dist/js/script.js
@@ -40,7 +40,17 @@ const html = document.querySelector('html');
 darkToggle.addEventListener('click', function() {
     if(darkToggle.checked) {
         html.classList.add('dark');
+        localStorage.theme = 'dark';
     }else {
         html.classList.remove('dark');
+        localStorage.theme = 'light';
     }
 });
+
+//memindahkan toggle otomatis ketika refresh
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked == 'true';
+  } else {
+    darkToggle.checked == 'false';
+  }
